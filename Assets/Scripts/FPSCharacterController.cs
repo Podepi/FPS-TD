34bf71aaa50fc7d26670572d72a4e54ce2c73b36
TarrielDRC <tarriel345@gmail.com>
2016-12-01 20:13:07 +1000

    Replace Standard Assets FPS character controller with custom mouse look and movement scripts. Add Crosshair.png
    Signed-off-by: TarrielDRC <tarriel345@gmail.com>

---
+++ Assets/Scripts/FPSCharacterController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FPSCharacterController : MonoBehaviour {
+
+	public float speed = 10.0f;
+	public float jumpSpeed = 100.0f;
+	public float gravMult = 1.0f;
+
+	public Rigidbody rb;
+
+	private float verticalVelocity = 0.0f;
+	private Vector3 moveDirection = Vector3.zero;
+
+	CharacterController cc;
+
+	// Use this for initialization
+	void Start () {
+		Cursor.lockState = CursorLockMode.Locked;
+		cc = GetComponent<CharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moveDirection = new Vector3 (Input.GetAxis ("Horizontal"), verticalVelocity, Input.GetAxis ("Vertical"));
+		moveDirection = transform.TransformDirection (moveDirection);
+		moveDirection *= speed;
+
+		if (Input.GetKeyDown ("escape")) {
+			Cursor.lockState = CursorLockMode.None;
+		}
+
+		if (cc.isGrounded) {
+			verticalVelocity = 0;
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				verticalVelocity = jumpSpeed;
+			}
+		}
+	}
+
+	void FixedUpdate() {
+		if (!cc.isGrounded) {
+			verticalVelocity += Physics.gravity.y * Time.deltaTime;
+		}
+		moveDirection.y = verticalVelocity;
+		cc.Move (moveDirection * Time.deltaTime);
+	}
+}
